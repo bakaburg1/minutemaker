ec52fff191de34411b41979314783153c5191d25
Angelo D'Ambrosio <bakaburg1@gmail.com>
2024-01-08 18:06:15 +0100

    feat: implement transcript summarisation

--- R/data_management.R
+++ R/data_management.R
@@ -1,1 +1,186 @@
 
+#' Extracts a sepecifoc talk text from a given transcript data
+#'
+#' The specific talk text can be identify using the relative agenda element or
+#' manually inputting the start and end times.
+#'
+#' @param transcript_data A data frame containing the transcript data. It must
+#'   have the following columns: 'start', 'end' and 'text'
+#' @param agenda_element A list with 'from' and 'to' fields indicating the start
+#'   and end times of the agenda element.
+#' @param start_time The start time for the extraction.
+#' @param end_time The end time for the extraction.
+#'
+#' @return A string containing the extracted text.
+#'
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+extract_text_from_transcript <- function(
+    transcript_data,
+    agenda_element = NULL,
+    start_time = NULL, end_time = NULL
+) {
+
+  # If no parameters are provided, the whole transcript is used and a warning is issued
+  if (is.null(agenda_element) && is.null(start_time) && is.null(end_time)) {
+    warning("The whole transcript will be used (may be very long)."
+            , call. = FALSE, immediate. = TRUE)
+  }
+
+  # If an agenda element is provided, its start and end times are used
+  if (!is.null(agenda_element)) {
+    start_time <- agenda_element$from
+    end_time <- agenda_element$to
+  }
+
+  # If no start time is provided, start from the beginning of the transcript
+  if (is.null(start_time)) {
+    start_time <- min(transcript_data$start)
+  }
+
+  # If no end time is provided, end at the end of the transcript
+  if (is.null(end_time)) {
+    start_time <- max(transcript_data$end)
+  }
+
+  # The data is filtered based on the start and end times, and the text is extracted
+  transcript_data %>%
+    filter(.data$start >= start_time, .data$end <= end_time) |>
+    filter(.data$text != "[...]") |>
+    with(paste(text, collapse = "\n"))
+}
+
+#' Builds IDs from each agenda item session and titles
+#'
+#' @param agenda A list containing the agenda items.
+#'
+#' @return A character vector containing the IDs for each agenda item.
+#'
+build_ids_from_agenda <- function(agenda) {
+  # Build the id of each talk using the session and title
+  talks_ids <- purrr::imap_chr(agenda, ~ {
+    # If the title is NULL, use an index as title
+    if (is.null(.x$title)) .x$title <- .y
+
+    paste0(
+      # If the session is not NULL, prepend it to the title
+      if (!is.null(.x$session)) paste0(.x$session, "_"),
+      .x$title
+    )
+  })
+}
+
+#' Formats a summary tree into a human-readable text
+#'
+#' The summary functions returns a machine-readable summary tree. This function
+#' takes the summary tree and formats it into a human-readable text.
+#'
+#' @param summary_tree A list containing the summary tree, or a string with the
+#'   path to a file containing the summary tree.
+#' @param agenda A list containing the agenda items.
+#' @param output_file A string with the path to the output file. If NULL, the
+#'   output is returned invisibly and not written to a file.
+#'
+#' @return A string containing the formatted summary tree, invisibly. This is printed to
+#'   the output file if one is provided.
+
+#' @export
+format_summary_tree <- function(
+    summary_tree,
+    agenda,
+    output_file = NULL
+) {
+
+  # If summary_tree is a file path, load the data from the file
+  if (is.character(summary_tree) && file.exists(summary_tree)) {
+    summary_tree <- dget(summary_tree)
+  }
+
+  # Initialize the output string
+  output <- ""
+
+  # Build the id of each talk using the session and title
+  talk_ids <- build_ids_from_agenda(agenda)
+
+  # Loop over each talk id
+  for (id in talk_ids) {
+    # Extract the summary for the current talk
+    talk_summary <- summary_tree[[id]]$summary
+    # Extract the agenda element for the current talk
+    agenda_element <- agenda[[which(talk_ids == id)]]
+
+    # If no summary is found for the current talk, issue a warning and skip to the next talk
+    if (is.null(talk_summary)) {
+      warning("No summary found for ", id, ". Skipping.", call. = FALSE)
+      next
+    }
+
+    # Generate a text version of the summary, with session, title, speakers, moderators and summary
+    output_piece <- with(agenda_element, {
+      speakers <- stringr::str_flatten_comma(speakers)
+      moderators <- stringr::str_flatten_comma(moderators)
+
+      stringr::str_glue("Session: {session};
+    Title: {title};
+    Speakers: {speakers};
+    Moderators: {moderators};
+    Summary:
+
+    {talk_summary}")
+    }) |>
+      stringr::str_remove_all("\\n?\\w+:\\s*;") # Remove empty fields
+
+    # Append the output piece to the output string
+    output <- paste0(output, output_piece, "\n\n####################\n\n")
+  }
+
+  # If an output file is specified, write the output to the file
+  if (!is.null(output_file)) {
+    readr::write_lines(output, output_file)
+  }
+
+  # Return the output string invisibly
+  invisible(output)
+}
+
+
+#' Validates an agenda element
+#'
+#' @param agenda_element A list containing the agenda element.
+#' @param session A boolean indicating whether the `session` item should be present.
+#' @param title A boolean indicating whether the `title` item should be present.
+#' @param speakers A boolean indicating whether the `speakers` item should be present.
+#' @param moderators A boolean indicating whether the `moderators` item should be present.
+#' @param type A boolean indicating whether the `type` item should be present.
+#' @param from A boolean indicating whether the `from` item should be present.
+#' @param to A boolean indicating whether the `to` item should be present.
+#'
+#' @return A boolean indicating whether the agenda element is valid.
+#'
+validate_agenda_element <- function(
+    agenda_element,
+    session = FALSE,
+    title = FALSE,
+    speakers = FALSE,
+    moderators = FALSE,
+    type = FALSE,
+    from = FALSE,
+    to = FALSE
+) {
+
+  # Get the arguments as a list
+  args <- as.list(environment())
+
+  # Remove the 'agenda_element' argument from the list
+  args$agenda_element <- NULL
+
+  # Check if the required items are present in the agenda element
+  is_valid <- purrr::imap_lgl(args, ~ {
+    !is.null(agenda_element[[.y]]) || isFALSE(.x)
+  }) |> all()
+
+  # Return the validation result
+  is_valid
+}
---
+++ R/prompts.R
@@ -0,0 +1,414 @@
+#' Set technical prompts that power the LLM model
+#'
+#' This function helps setting the prompts for the LLM that is tasked with
+#' summarising meetings and conference presentations. The prompts are set as
+#' options in the R environment. If `NULL` is passed as an argument, the default
+#' prompts are used.`
+#'
+#' @param base_task The base task of the AI secretary.
+#' @param transcript The transcript to be summarised.
+#' @param vocabulary The vocabulary to be used.
+#' @param diarization_instructions Instructions for diarization.
+#' @param extra_diarization_instructions Extra instructions for diarization.
+#' @param audience_instructions Instructions for the audience and the summary
+#'   focus.
+#' @param audience The audience for the summary.
+#' @param summarisation_focus The focus of the summarisation.
+#' @param output_format The format of the output.
+#' @param presentation_details The details of the presentation.
+#' @param meeting_description The description of the meeting.
+#' @param force If TRUE, overwrite existing prompts.
+#'
+#' @return Nothing. The prompts are set as options in the R environment.
+#'
+#' @importFrom stats setNames
+#'
+#' @export
+#'
+set_prompts <- function(
+    base_task = NULL,
+    transcript = NULL,
+    vocabulary = NULL,
+    diarization_instructions = NULL,
+    extra_diarization_instructions = NULL,
+    audience_instructions = NULL,
+    audience = NULL,
+    summarisation_focus = NULL,
+    output_format = NULL,
+    presentation_details = NULL,
+    meeting_description = NULL,
+
+    force = TRUE
+) {
+
+  # Get the arguments as a list
+  args <- as.list(environment())
+
+  # Remove the 'force' argument from the list
+  args$force <- NULL
+
+  # Define a function to collapse the prompt pieces into a single string
+  collapse <- purrr::partial(paste, sep = "\n")
+
+
+  defaults <- list(
+    base_task = collapse(
+      "You are an AI secretary, expert in summarising meetings and conference presentations.",
+      "Your task is to provide a summary of the transcripts that will be provided to you.",
+      "Take into account that multiple speakers may be present in the transcript {diarization_instructions}."
+    ),
+
+    diarization_instructions = ", each idenfitied by a specific (i.e., a name) or generic (e.g. Guest 1) label. Keep into account that the diarization is never perfect, therefore the same speaker may be identified with different labels in different parts of the transcript",
+
+    meeting_description = collapse(
+      "The following is a description of the meeting:",
+      "<meeting_description>", "{event_description}", "</meeting_description>"
+    ),
+
+    vocabulary = "Mind that the transcript is not perfect and the following and other terms and names may have been wrongly transcribed: {vocabulary}. Please correct them.",
+
+    transcript = collapse(
+      "Here is the transcript:",
+      "<transcript>", "{transcript}", "</transcript>"
+      ),
+
+    audience_instructions = "Your summary must focus on aspects relevant for an audience with the following characteristics:\n<audience>{audience}</audience>",
+
+    audience = "An audience with understanding of the topic",
+
+    summarisation_focus = collapse(
+      "Your summary must include the following sections:",
+      "<summary_sections>",
+      "- Topic of the talk/meeting: Identify the main topic(s) discussed. Understanding the objective will guide you in determining which parts of the presentation are most crucial to include in your summary.",
+      "- Key points: Identify all possible key points/results/take home messages of the presentation/meeting. These are the main ideas that the speaker wants to convey to the audience.",
+      "- Questions and Discussions: If there were interventions from the audience, include key questions and answers or points of discussion. This often provides additional insight and clarifies any ambiguities in the presentation. Try to infer the author of the question from the transcript even if they don't mention their name {extra_diarization_instructions}.",
+      "- Conclusions: Summarize the main conclusions of the presentation/meeting. If the speaker doesn't mention the conclusions explicitly, try to infer them from the context of the presentation/meeting. List any discussed action points if mentioned during the presentation/meeting.",
+      "</summary_sections>"
+    ),
+
+    extra_diarization_instructions = "; if the speaker label is generic (e.g. Guest 1), try to infer the name from the context",
+
+    output_format = collapse(
+      "<output_instructions>",
+      "When summarizing, aim to be concise but extremely comprehensive.",
+      "Use only the information provided in the transcript, do not add any details not mentioned in the transcript.",
+      "Speak at the present tense when describing what was said during the talk/meeting.",
+      "The summary must provide a clear and extremely information-dense representation of the transcript, without unnecessary details and formalisms.",
+      "Avoid reporting circumstantial details, jokes, non-content related comments, etc.",
+      "It must allow the reader to internalize most of the information of the original recording without reading the full transcript.",
+      "Remember to maintain the original meaning and context of the presentation/discussion.",
+      "Your summary must be one-two pages long.",
+      "</output_instructions>"
+    )
+  )
+
+  # Walk through the names of the default prompts
+  purrr::walk(names(defaults), function(prompt_name) {
+    # Create the option label
+    opt_label <- paste0("minutemaker_prompt_", prompt_name)
+
+    # Get the current prompt
+    current_prompt <- getOption(opt_label)
+
+    # Get the prompt from the arguments
+    prompt <- args[[prompt_name]]
+
+    # If the prompt is NULL, use the default prompt
+    if (is.null(prompt)) {
+      prompt <- defaults[[prompt_name]]
+    }
+
+    # If the current prompt is NULL or 'force' is TRUE, set the prompt
+    if (is.null(current_prompt) || force) {
+      prompt |>
+        list() |>
+        setNames(opt_label) |>
+        options()
+    }
+  })
+}
+
+#' Get the current prompts
+#'
+#' @return A list containing the current prompts.
+#'
+#' @export
+get_prompts <- function() {
+  options()[grepl("minutemaker_prompt_", names(options()))]
+
+}
+
+#' Generate Recording Details
+#'
+#' This function generates a formatted string containing details about a
+#' recording. The details include the type of talk/meeting, the session in which
+#' it took place, the title of the talk/meeting, the main speakers, and the
+#' moderators. All parameters are optional.
+#'
+#' @param agenda_element A list containing the details of the recording. If this
+#'   is provided, the other parameters are ignored.
+#' @param type The type of the talk/meeting, e.g., "conference talk",
+#'   "workshop", "panel discussion", "conference welcome message", etc.
+#' @param session The session in which the talk/meeting took place.
+#' @param title The title of the talk/meeting.
+#' @param speakers The main speakers of the talk/meeting as a vector.
+#' @param moderators The moderators of the talk/meeting as a vector.
+#'
+#' @return A formatted string containing the details of the recording, or NULL
+#'   if all parameters are NULL.
+#'
+#' @export
+generate_recording_details <- function(
+    agenda_element = NULL,
+    type = NULL,
+    session = NULL,
+    title = NULL,
+    speakers = NULL,
+    moderators = NULL
+) {
+
+  # If agenda_element is not NULL, extract the information from it
+  if (!is.null(agenda_element)) {
+    type <- agenda_element$type
+    session <- agenda_element$session
+    title <- agenda_element$title
+    speakers <- agenda_element$speakers
+    moderators <- agenda_element$moderators
+  }
+
+  # Return NULL if all the arguments are NULL
+  if (is.null(type) && is.null(session) &&
+      is.null(title) && is.null(speakers) && is.null(moderators)) {
+    return(NULL)
+  }
+
+  # Helper function to add a section to the details string if it is not NULL
+  add_section <- function(section, section_name) {
+    if (!is.null(section)) {
+      paste0(
+        "- ", stringr::str_flatten_comma(section_name),
+        ": ", section, ";\n")
+    }
+  }
+
+  # Build the details prompt based on which arguments are not NULL
+  paste0(
+    "The following are the information on the recording: <recording_details>\n",
+    add_section(type, "Type (the type of this specific talk/meeting)"),
+    add_section(session, "Session (the conference/larger meeting session in which this talk/meeting took place)"),
+    add_section(title, "Title (the title of the talk/meeting)"),
+    add_section(speakers, "Speakers (the main speakers of this talk/meeting)"),
+    add_section(moderators, "Moderators (the moderators of this talk/meeting)"),
+    "</recording_details>"
+  )
+}
+
+#' Summarise a recording transcript
+#'
+#' This function takes a transcript and various optional parameters, and returns
+#' a summarised version of the transcript. All parameters apart from the
+#' transcript are optional, and if not provided they will be ignored in the
+#' final prompt.
+#'
+#' @param transcript A character vector representing the transcript to be
+#'   summarised.
+#' @param event_description A description of the event.
+#' @param event_details Additional details about the event. We suggest using
+#'   `generate_recording_details` to generate this string.
+#' @param audience A description the intended audience and the focus of the
+#'   summarisation.
+#' @param vocabulary A character vector of specific vocabulary words, names,
+#'   definitions, to help the LLM recognise misspellings and abbreviations.
+#' @param is_diarized A logical indicating whether the transcript is diarized.
+#'   Default is FALSE.
+#' @param extra_diarization_instructions Additional diarization instructions,
+#'   e.g., "Do not report the name of the speaker", "Use only the second name of
+#'   the speaker", etc...
+#' @param ... Additional arguments passed to the `interrogate_llm` function,
+#'   such as the LLM provider.
+#'
+#' @return A summary of the transcript.
+#'
+#' @export
+#'
+summarise_transcript <- function(
+    transcript,
+    event_description = NULL,
+    event_details = NULL,
+    audience = NULL,
+    vocabulary = NULL,
+    is_diarized = FALSE,
+    extra_diarization_instructions = NULL,
+    ...
+) {
+
+  transcript <- paste(transcript, collapse = "\n")
+
+  # Helper function to get the prompt options
+  get_ops <- function(ops) {
+    ops <- paste0("minutemaker_prompt_", ops)
+
+    getOption(ops, NULL)
+  }
+
+  diarization_instructions <- NULL
+
+  if (is_diarized) {
+    diarization_instructions <- get_ops("diarization_instructions")
+  }
+
+  # Build the prompt
+  prompt <- paste(
+    get_ops("base_task"),
+
+    if (!is.null(event_description)) {
+      get_ops("event_description")
+    },
+
+    event_details,
+
+    get_ops("transcript"),
+
+    if (!is.null(vocabulary)) {
+      vocabulary <- stringr::str_flatten_comma(vocabulary)
+      get_ops("vocabulary")
+    },
+
+    get_ops("summarisation_focus"),
+
+    if (!is.null(audience)) {
+      audience <- stringr::str_flatten_comma(audience)
+      get_ops("audience_instructions")
+    },
+
+    get_ops("output_format"),
+
+    sep = "\n"
+  ) |>
+    stringr::str_replace_all("\n\n+", "\n\n") |> # remove multiple newlines
+    stringr::str_glue(.null = NULL) # .null default makes the output character(0) if any of the {vars} is NULL
+
+  interrogate_llm(
+    c(user = prompt),
+    ...
+  )
+}
+
+#' Summarise a full meeting
+#'
+#' This function summarises a full meeting by processing the transcript data and
+#' the agenda. It generates a summary for each talk in the meeting and saves the
+#' results in an output file. It's built on top of `summarise_transcript`.
+#'
+#' @param transcript_data The transcript data of the meeting as a data frame.
+#' @param agenda The agenda of the meeting, that is, a list of agenda elements
+#'   each with a session name, a title, speaker and moderator lists, type of
+#'   talk and start and end times.
+#' @param output_file The file to save the results to as a list. Default is
+#'   "event_summary.R".
+#' @param audience The audience of the meeting. See `summarise_transcript` for
+#'   more details.
+#' @param meeting_description The description of the meeting. See
+#'   `summarise_transcript` for more details.
+#' @param meeting_audience The audience of the meeting. See
+#'   `summarise_transcript` for more details.
+#' @param vocabulary The vocabulary used in the meeting. See
+#'   `summarise_transcript` for more details.
+#' @param is_diarized Whether the meeting is diarized. See
+#'   `summarise_transcript` for more details.
+#' @param extra_diarization_instructions Extra instructions for diarization. See
+#'   `summarise_transcript` for more details.
+#' @param overwrite Whether to overwrite existing summaries. Default is FALSE.
+#' @param ... Additional arguments passed to `interrogate_llm` function, such as
+#'   the LLM provider.
+#'
+#' @return The result tree of the meeting summary. Also saves the results in the
+#'   output file as side effect.
+#'
+#' @export
+summarise_full_meeting <- function(
+    transcript_data,
+    agenda,
+    output_file = "event_summary.R",
+
+    audience = NULL,
+    meeting_description = NULL,
+    meeting_audience = NULL,
+    vocabulary = NULL,
+    is_diarized = FALSE,
+    extra_diarization_instructions = NULL,
+
+    overwrite = FALSE,
+    ...
+) {
+
+  # Generate the output container if it doesn't exist
+  if (!file.exists(output_file)) {
+    result_tree <- list()
+  } else {
+    result_tree <- dget(output_file)
+  }
+
+  # Build the id of each talk using the session and title
+  talks_ids <- build_ids_from_agenda(agenda)
+
+  for (id in talks_ids) {
+    # Skip if the talk has already been summarised
+    if (!is.null(result_tree[[id]]) && overwrite) {
+      next
+    }
+
+    # Get the index of the current talk in the agenda
+    i <- which(talks_ids == id)
+
+    # Get the agenda element of the current talk
+    agenda_element <- agenda[[i]]
+
+    # Validate the agenda element to make sure it has a start and end time
+    is_valid_agenda <- validate_agenda_element(
+      agenda_element,
+      from = TRUE, to = TRUE
+    )
+
+    if (!is_valid_agenda) {
+      warning("The agenda element ", id, " is not valid. Skipping.",
+              call. = FALSE, immediate. = TRUE)
+      next
+    }
+
+    # Extract the transcript of the talk
+    transcript_text <- extract_text_from_transcript(
+      transcript_data,
+      agenda_element = agenda_element
+    )
+
+    # Extract the details of the talk
+    event_details <- generate_recording_details(agenda_element)
+
+    # Extract the vocabulary of the talk
+    talk_summary <- summarise_transcript(
+      transcript = transcript_text,
+      event_description = meeting_description,
+      event_details = event_details,
+      vocabulary = vocabulary,
+      audience = meeting_audience,
+      ...
+    )
+
+    # Create the output object for the current talk
+    output <- list(
+      session = agenda_element$session,
+      title = agenda_element$title,
+      summary = talk_summary
+    )
+
+    # Add the output to the results tree
+    result_tree[[id]] <- output
+
+    # Update the results file
+    dput(result_tree, file = output_file)
+  }
+
+  # Return the result tree invisibly
+  invisible(result_tree)
+}
